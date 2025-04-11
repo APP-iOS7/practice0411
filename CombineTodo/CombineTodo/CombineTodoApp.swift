796b835da6a64d3ab259fc03e843e01f80d5a709
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-11 09:38:38 +0900

    Combine Todo 프로젝트 생성

---
+++ CombineTodo/CombineTodo/CombineTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTodoApp.swift
+//  CombineTodo
+//
+//  Created by KimJunsoo on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
