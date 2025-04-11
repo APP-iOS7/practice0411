7520b799675ad2ea43e8313d7238df8b03219a72
hiereit-dev <https://github.com/hiereit-dev>
2025-04-11 10:05:38 +0900

    WeatherService 추가, enum파일 추가, WeatherKit capability 추가

---
+++ WeatherApp/WeatherApp/Enums.swift
@@ -0,0 +1,12 @@
+//
+//  Enums.swift
+//  WeatherApp
+//
+//  Created by 박세라 on 4/11/25.
+//
+
+import Foundation
+
+enum WeatherError: Error {
+    case dataUnavailable
+}
---
+++ WeatherApp/WeatherApp/WeatherService.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by 박세라 on 4/11/25.
+//
+
+
+import Foundation
+import WeatherKit
+import CoreLocation
+
+public class WeatherService {
+
+    private let service = WeatherKit.WeatherService()
+    
+    func fetchWeather(location: CLLocation) async throws -> WeatherData {
+        let weather = try await service.weather(for: location)
+        let currentWeather = weather.currentWeather
+        
+        return WeatherData(
+            temperature: currentWeather.temperature.value,
+            description: currentWeather.condition.description,
+            humidity: currentWeather.humidity,
+            windSpeed: currentWeather.wind.speed.value
+        )
+    }
+}
