8d0300bd4ea005e2d91c00c0b9130c33ff7faf3d
hiereit-dev <https://github.com/hiereit-dev>
2025-04-11 09:36:59 +0900

    WeatherData 추가

---
+++ WeatherApp/WeatherApp/WeatherData.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by 박세라 on 4/11/25.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    var temperature: Double
+    var description: String
+    var humidity: Double
+    var windSpeed: Double
+}
