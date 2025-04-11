8f74c8dc65a21cea37ca565e57ab900d39c8eb9f
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-11 10:13:03 +0900

    날씨 데이터 모델 추가

---
+++ WeatherApp/WeatherApp/Models/WeatherData.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by KimJunsoo on 4/11/25.
+//
+
+import Foundation
+
+// MARK: - 날씨 데이터 모델
+struct WeatherData: Codable {
+    let temperature: Double // 섭씨 온도
+    let description: String // 날씨 설명
+    let humidity: Double // 습도
+    let windSpped: Double // 풍속
+}
