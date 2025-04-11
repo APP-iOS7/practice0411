0291de3a9c6c885ffc0c20e663d7fb2742f449f0
cty7574 <vicpool@naver.com>
2025-04-11 10:30:23 +0900

    info.plist 세팅, weatherdata 모델 추가

---
+++ WeatherCombine/WeatherCombine/Models/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  WeatherCombine
+//
+//  Created by 멘태 on 4/11/25.
+//
+
+struct WeatherData: Codable {
+    let temperature: Double
+    let description: String
+    let humidity: Double
+    let windSpeed: Double
+}
