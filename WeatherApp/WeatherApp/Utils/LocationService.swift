12a14464020ef6a91a9d69a71efc6993868df7fa
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-11 16:17:22 +0900

    LocationService 수정, Delegate 작성

--- WeatherApp/WeatherApp/Utils/LocationService.swift
+++ WeatherApp/WeatherApp/Utils/LocationService.swift
@@ -9,16 +9,17 @@ import Foundation
 import CoreLocation
 
 // MARK: - 위치 서비스 클래스
-class LocationService: NSObject {
+class LocationService: NSObject, ObservableObject {
     
-    static let shared = LocationService()
     private let locationManager = CLLocationManager()
-    weak var delegate: (any CLLocationManagerDelegate)?
-    private var currentLocation: CLLocation?
+    
+    @Published var location: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
     
     override init() {
         super.init()
         setupLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
     }
     
     // 위치 매니저 초기 설정
@@ -44,22 +45,33 @@ class LocationService: NSObject {
         locationManager.stopUpdatingLocation()
     }
     
+    // CLLocation 위치 반환
     func location(latitude: Double, longitude: Double) -> CLLocation {
         return CLLocation(latitude: latitude, longitude: longitude)
     }
-}
-
-// TODO: - 위치 서비스 클래스 CLLocationManagerDelegate
-extension LocationService: CLLocationManagerDelegate {
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        guard abs(location.timestamp.timeIntervalSinceNow) < 3600 else { return }
-        
-        locationManager.stopUpdatingLocation()
+    // 현재 위치 요청
+    func requestLocation() {
+        locationManager.requestLocation()
     }
     
+    
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    // 위치 업데이트 시 호출
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+    }
+    
+    // 위치 업데이트 실패 시 호출
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        
+        print("🔴 위치 업데이트 실패: \(error.localizedDescription)")
+    }
+    
+    // 권한 상태 변경 시 호출
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        authorizationStatus = status
     }
 }
