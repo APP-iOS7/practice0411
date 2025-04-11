db3b9a233e789690721af5ceffa7272d3e890f3d
cty7574 <vicpool@naver.com>
2025-04-11 09:44:31 +0900

    프로젝트 생성

---
+++ WeatherCombine/WeatherCombine/WeatherCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCombineApp.swift
+//  WeatherCombine
+//
+//  Created by 멘태 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
