5b620647ec0d7364e2fc89dea1af04a7cca637a0
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-11 11:45:57 +0900

    WeatherData 모델 추가 및 WeatherKit 기본셋팅

---
+++ WeatherKitPractice/WeatherKitPractice/Models/WeatherData.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherData.swift
+//  WeatherKitPractice
+//
+//  Created by NoelMacMini on 4/11/25.
+//
+import Foundation
+
+struct WeatherData: Codable {
+  let Temperature: Double  // 섭씨 온도
+  let description: String  // 날씨 설명
+  let humidity: Double     // 습도
+  let windSpeed: Double    // 풍속
+}
