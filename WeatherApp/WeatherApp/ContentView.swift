da139edacd28ad818b84a1a66f45840796b04ff0
hiereit-dev <https://github.com/hiereit-dev>
2025-04-11 11:24:53 +0900

    ContentView생성 메인뷰 구현 - 날씨정보 표시 - 새로고침버튼

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    
+    //@StateObject private var viewModel = WeatherViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    HStack {
+                        /*
+                        TextField($viewModel.location, text: "dd")
+                         */
+                    }
+                    HStack {
+                        Text("🌡️")
+                        Text("온도")
+                        Spacer()
+                        Text("7")
+                    }
+                    HStack {
+                        Text("ℹ")
+                        Text("정보")
+                        Spacer()
+                        Text("")
+                    }
+                    HStack {
+                        Text("🥵")
+                        Text("습도")
+                        Spacer()
+                        Text("17%")
+                    }
+                    HStack {
+                        Text("💨")
+                        Text("풍속")
+                        Spacer()
+                        Text("7ms")
+                    }
+                }
+            }.navigationTitle("Weather")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            print("버튼 눌림!")
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                        }
+                    }
+                }
         }
-        .padding()
     }
 }
 
