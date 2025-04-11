73b6f947363dae16293c452a91341d1719f7c084
kdy <kokbael@naver.com>
2025-04-11 10:01:31 +0900

    WeatherApp 프로젝트 생성

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 김동영 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
